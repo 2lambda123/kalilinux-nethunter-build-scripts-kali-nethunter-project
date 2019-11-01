13d6a51a84d93d5e047cfdebbf354a08d32d822c
Re4son <3520622-re4son@users.noreply.gitlab.com>
2019-11-01 19:25:00 +1100

    Update pkg_full list
    Signed-off-by: Re4son <3520622-re4son@users.noreply.gitlab.com>

--- nethunter-fs/build.sh
+++ nethunter-fs/build.sh
@@ -148,11 +148,12 @@ pkg_minimal="openssh-server kali-defaults kali-archive-keyring
 	apt-transport-https ntpdate usbutils pciutils sudo vim"
 
 # DEFAULT PACKAGES FULL INSTALL
-pkg_full="kali-linux-nethunter mana-toolkit exploitdb msfpc
-	exe2hexbat bettercap libapache2-mod-php7.3 libreadline6-dev
-	libncurses5-dev libnewlib-arm-none-eabi binutils-arm-none-eabi
-	gcc-arm-none-eabi autoconf libtool make gcc-7 g++-7
-	libxml2-dev zlib1g-dev libncurses5-dev"
+pkg_full="kali-linux-nethunter
+          kali-desktop-core xfce4-terminal xserver-xorg-input-synaptics gnome-theme-kali mousepad
+          mana-toolkit msfpc exe2hexbat bettercap
+          libapache2-mod-php7.3 libreadline6-dev libncurses5-dev libnewlib-arm-none-eabi
+          binutils-arm-none-eabi gcc-arm-none-eabi autoconf libtool make gcc-7 g++-7
+          libxml2-dev zlib1g-dev"
 
 # ARCH SPECIFIC PACKAGES
 pkg_minimal_armhf="abootimg cgpt fake-hwclock vboot-utils vboot-kernel-utils nethunter-utils"
